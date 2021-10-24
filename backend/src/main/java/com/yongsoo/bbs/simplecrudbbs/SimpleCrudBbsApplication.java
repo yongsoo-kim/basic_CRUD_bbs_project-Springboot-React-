d82bef1cf81c844489511f8a85c7be30a05f1b2a
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-10-24 16:43:26 +0900

    feature: add backend basic modules.

---
+++ backend/src/main/java/com/yongsoo/bbs/simplecrudbbs/SimpleCrudBbsApplication.java
@@ -0,0 +1,13 @@
+package com.yongsoo.bbs.simplecrudbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudBbsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yongsoo/bbs/simplecrudbbs/SimpleCrudBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yongsoo.bbs.simplecrudbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
